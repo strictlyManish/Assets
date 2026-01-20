2d4c04592827e33370eec7ea74cfa2074a56e94b
strictlyManish <rajzmanish94@gmail.com>
2026-01-20 12:34:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-gray-900 text-white w-full h-screen p-5'>
+      <h1>React-Router-Dom</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/routes/Mainroutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Mainroutes() {
+  return (
+    <div>Mainroutes</div>
+  )
+}
+
+export default Mainroutes
